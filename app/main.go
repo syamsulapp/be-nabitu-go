dd87e1ab5ce294a70b1cc1d47ccc3ede0c5874d2
samsul marif <samsul.marif100@gmail.com>
2024-05-23 06:34:49 +0800

    log fatallin

--- app/main.go
+++ app/main.go
@@ -17,5 +17,5 @@ func main() {
 	SubRouter := router.PathPrefix("api/v1").Subrouter()
 	routes.InitRouteProfile(router)
 
-	log.Fatal(http.ListenAndServe(os.Getenv("APP_PORT"), SubRouter))
+	log.Fatalln(http.ListenAndServe(os.Getenv("APP_PORT"), SubRouter))
 }
