1f0753b9f09cd3db2396e16cded82fe99e5e9f0a
samsul marif <samsul.marif100@gmail.com>
2024-05-31 19:48:31 +0800

    fix migrations

--- database/migrations/20240531112736_create_table_nabitu.up.sql
+++ database/migrations/20240531112736_create_table_nabitu.up.sql
@@ -1,25 +1,25 @@
 CREATE TABLE users
 (
-    id          INT             NOT NULL AUTO_INCREMENT,
-    fullname    VARCHAR(128)    NOT NULL,
-    age         VARCHAR(128)    NOT NULL,
-    alamat      VARCHAR(128)    NOT NULL,
-    createt_at  TIMESTAMP       NULL,
-    updated_at  TIMESTAMP       NULL,
-    deleted_at  TIMESTAMP       NULL,
+    id              INT             NOT NULL AUTO_INCREMENT,
+    username        VARCHAR(128)    NOT NULL,
+    email           VARCHAR(128)    NOT NULL,
+    password        VARCHAR(128)    NOT NULL,
+    createt_at      TIMESTAMP       NULL,
+    updated_at      TIMESTAMP       NULL,
+    deleted_at      TIMESTAMP       NULL,
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
 CREATE TABLE profiles
 (
-    id              INT             NOT NULL AUTO_INCREMENT,
-    username        VARCHAR(128)    NOT NULL,
-    email           VARCHAR(128)    NOT NULL,
-    password        VARCHAR(128)    NOT NULL,
-    users_id        INT             NOT NULL,
-    createt_at      TIMESTAMP       NULL,
-    updated_at      TIMESTAMP       NULL,
-    deleted_at      TIMESTAMP       NULL,
+    id          INT             NOT NULL AUTO_INCREMENT,
+    fullname    VARCHAR(128)    NOT NULL,
+    age         VARCHAR(128)    NOT NULL,
+    alamat      VARCHAR(128)    NOT NULL,
+    users_id    INT             NOT NULL,
+    createt_at  TIMESTAMP       NULL,
+    updated_at  TIMESTAMP       NULL,
+    deleted_at  TIMESTAMP       NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (users_id) REFERENCES users(id)
 )ENGINE = InnoDB;
