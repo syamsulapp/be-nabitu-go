861ddabcc9d417e81920ca7dfbb120f62fdea941
samsul marif <samsul.marif100@gmail.com>
2024-06-07 21:18:37 +0800

    test config cors

--- app/main.go
+++ app/main.go
@@ -44,7 +44,12 @@ func indexRoute(c *fiber.Ctx) error {
 
 func SetupRouter() *fiber.App {
 	app := fiber.New()
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "https://*.mjscode.pro",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	app.Get("/", indexRoute)
 	routes.InitRouteAPI(app)
 	return app
